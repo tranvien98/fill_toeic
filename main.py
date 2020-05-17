3eb96c73fbe66b5dc5a1bcfcf13f7ef94f3c77c8
TranVien <tranvanvien98bg@gmail.com>
2020-05-17 22:59:36 +0700

    add file app.yaml

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import requests
 import re
 app = Flask(__name__)
 
-# fill_bert = FillBert()
+fill_bert = FillBert()
 @app.route('/')
 def home():
     
@@ -20,7 +20,7 @@ def result():
     for i in range(1,5):
         print(questions[i])
         row[str(i)] = re.sub('</?.*?>','',questions[i].replace('(','<').replace(')', '>')).strip()
-    print(row)
+    row['answer'] = fill_bert.predict(row)
     return render_template('result.html', row=row)
 @app.route('/example')
 def example():
--- predict.py
+++ predict.py
@@ -4,28 +4,26 @@ from pytorch_pretrained_bert import BertTokenizer, BertModel, BertForMaskedLM
 
 class FillBert():
     def __init__(self):
-        use_cuda = torch.cuda.is_available()
-        device = torch.device("cuda" if use_cuda else "cpu")
-        bertmodel = 'bert-large-uncased'
-        tokenizer = BertTokenizer.from_pretrained(bertmodel)
-        model = BertForMaskedLM.from_pretrained(bertmodel).to(device)
-        model.eval()
+        self.use_cuda = torch.cuda.is_available()
+        self.device = torch.device("cuda" if self.use_cuda else "cpu")
+        self.bertmodel = 'bert-large-uncased'
+        self.tokenizer = BertTokenizer.from_pretrained(self.bertmodel)
+        self.model = BertForMaskedLM.from_pretrained(self.bertmodel).to(self.device)
+        self.model.eval()
     def get_score(self,question_tensors, segment_tensors, masked_index, candidate):
-        candidate_tokens = tokenizer.tokenize(
-            candidate)  # warranty -> ['warrant', '##y']
-        candidate_ids = tokenizer.convert_tokens_to_ids(candidate_tokens)
-        predictions = model(question_tensors, segment_tensors)
-        predictions_candidates = predictions[0,
-                                             masked_index, candidate_ids].mean()
+        candidate_tokens = self.tokenizer.tokenize(candidate)  # warranty -> ['warrant', '##y']
+        candidate_ids = self.tokenizer.convert_tokens_to_ids(candidate_tokens)
+        predictions = self.model(question_tensors, segment_tensors)
+        predictions_candidates = predictions[0,masked_index, candidate_ids].mean()
         return predictions_candidates.item()
     def predict(self,row):
-        question_tokens = tokenizer.tokenize(row['question'].replace('___', '_'))
+        question_tokens = self.tokenizer.tokenize(row['question'].replace('___', '_'))
         masked_index = question_tokens.index('_')
         question_tokens[masked_index] = '[MASK]'
         segment_ids = [0] * len(question_tokens)
-        segment_tensors = torch.tensor([segment_ids]).to(device)
-        question_ids = tokenizer.convert_tokens_to_ids(question_tokens)
-        question_tensors = torch.tensor([question_ids]).to(device)
+        segment_tensors = torch.tensor([segment_ids]).to(self.device)
+        question_ids = self.tokenizer.convert_tokens_to_ids(question_tokens)
+        question_tensors = torch.tensor([question_ids]).to(self.device)
         candidates = [row['1'], row['2'], row['3'], row['4']]
         predict_tensor = torch.tensor([self.get_score(question_tensors, segment_tensors,
                                                 masked_index, candidate) for candidate in candidates])
